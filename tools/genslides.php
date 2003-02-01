422b6f7c42aea0e7fd36f3eb9090e17e9fb91c8c
Sterling Hughes <sterling@php.net>
2003-02-01 02:02:37 +0000

    add a bit of code that will take presentations defined in one big file and spit them out into a organized presentation directory.
    # The gauntlet for ugly code has been thrown down

---
+++ tools/genslides.php
@@ -0,0 +1,131 @@
+#!/usr/local/bin/php -q
+<?php
+class presentation_slide {
+	var $data;
+	var $title;
+	var $filename;
+}
+
+
+class presentation_generator {
+	var $slides;
+	var $presfile;
+	var $presfileStart;
+	var $directory;
+
+	function presentation_generator ($filename, $directory, $presfile)
+	{
+		$this->directory = $directory;
+		$this->presfile = $presfile;
+		print "parsing...";
+		$tmp = $this->parseFile($filename);
+		$this->presfileStart = $tmp['start'];
+		unset($tmp['start']);
+		$this->slides = $tmp;
+		print "done!\n";
+	}
+
+	function parseFile ($filename)
+	{
+		$fp = fopen($filename, "r");
+		
+		$inslide = 0;
+		$beginning = 1;
+		$element = 0;
+		$buf = "";
+		$start = "";
+		$data = array();
+
+		while (!feof($fp)) {
+			$line = fgets($fp);
+
+			if ($inslide) {
+				if (preg_match('@</slide>@', $line)) {
+					$data[$element]->data = $buf;
+	
+					++$element;
+					$inslide = 0;
+	
+					continue;
+				}
+
+				$buf .= $line;
+			}
+				
+			if (!$inslide && preg_match('/^<slide.*>/', $line) && 
+			    preg_match_all('/(title|filename)\=\"([\w\.\s]+)\"/', $line, $matches)) {
+				$beginning = 0;
+				$inslide = 1;
+				$data[$element] = new presentation_slide;
+
+				if (isset($matches[1][0])) {
+					$data[$element]->{$matches[1][0]} = $matches[2][0];
+				} else {
+					$this->warn("No title found.. Assigning 'unknown'\n");
+					$data[$element]->title = "unknown";
+				}
+
+				if (isset($matches[1][1])) {
+					$data[$element]->{$matches[1][1]} = $matches[2][1];
+				}
+			}
+
+			if ($beginning) {
+				$start .= $line;
+			}
+		}
+
+		$data['start'] = $start;
+
+		return $data;
+	}
+
+	function createFilename($title) {
+		return str_replace(" ", "_", strtolower($title)). '.xml';
+	}
+
+	function xmlEscape($data) {
+		return htmlspecialchars($data);
+	}
+
+	function generate_slides() {
+		$slides = &$this->slides;
+
+		print "generating slides...\n";
+		for ($i = 0, $j = count($this->slides); $i < $j; ++$i) {
+			$slide = &$this->slides[$i];
+			
+			if (empty($slide->filename)) {
+				$slide->filename = $this->createFilename($slide->title);
+			}
+			$slide->filename = "{$this->directory}/{$slide->filename}";
+			print "Generating ({$slide->title}): {$slide->filename}...";
+
+			$fp = fopen($slide->filename, "w");
+			fwrite($fp, '<slide title="' . $this->xmlEscape($slide->title) . '">');
+			fwrite($fp, $slide->data);
+			fwrite($fp, '</slide>');
+			fclose($fp);
+
+			print " done!\n";
+		}
+		print "slides generated!\n";
+	}
+
+	function generate_presentation() {
+		print "generating presentation file: {$this->presfile}...";
+		$fp = fopen($this->presfile, "w");
+		fwrite($fp, $this->presfileStart);
+		foreach ($this->slides as $slide) {
+			fwrite($fp, "<slide>{$slide->filename}</slide>\n");
+		}
+		fwrite($fp, "</presentation>");
+		print " done!\n";
+	}
+		
+}
+
+$p = new presentation_generator($_SERVER['argv'][1], $_SERVER['argv'][2], $_SERVER['argv'][3]);
+$p->generate_slides();
+$p->generate_presentation();
+?>
